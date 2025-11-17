77bfd22f7d5d2ac830fc74695ffd2dcd0243d201
Webpolka <webpolka@gmail.com>
2025-11-17 17:27:53 +0300

    second

--- docs/vendor/js/helpers.js
+++ docs/vendor/js/helpers.js
@@ -1,29 +1,29 @@
 /* -------------------------------------------------------------------------------------------------------------------------------------------------
 AutoREM - функция для установки масштабирования в автоматическом режиме (на всю ширину экрана) 
 -----------------------------------------------------------------------------------------------------------------------------------------------------*/
-function autoREM(baseSiteWidth, baseFontSize){
-    const htmlElement = document.documentElement;
+function autoREM(baseSiteWidth, baseFontSize) {
+	const htmlElement = document.documentElement;
 
-    function updateFontSize() {
-        const screenWidth = window.innerWidth;
-        // Вычисляем масштабный коэффициент
-        const scaleFactor = screenWidth / baseSiteWidth;
-        // Новой размер шрифта
-        const newFontSize = baseFontSize * scaleFactor;
+	function updateFontSize() {
+		const screenWidth = window.innerWidth;
+		// Вычисляем масштабный коэффициент
+		const scaleFactor = screenWidth / baseSiteWidth;
+		// Новой размер шрифта
+		const newFontSize = baseFontSize * scaleFactor;
 
-        if (screenWidth >= baseSiteWidth) {
-            // Устанавливаем размер шрифта для <html>
-            htmlElement.style.fontSize = `${newFontSize}px`;
-        }
-    }
+		if (screenWidth >= baseSiteWidth) {
+			// Устанавливаем размер шрифта для <html>
+			htmlElement.style.fontSize = `${newFontSize}px`;
+		}
+	}
 
-    // Обновляем при загрузке и при изменении окна
-    window.addEventListener("resize", updateFontSize);
-    // Инициализация при загрузке страницы
-    updateFontSize();
+	// Обновляем при загрузке и при изменении окна
+	window.addEventListener("resize", updateFontSize);
+	// Инициализация при загрузке страницы
+	updateFontSize();
 };
 
-// autoREM(1440, 16);
+autoREM(1440, 16);
 
 
 /* -------------------------------------------------------------------------------------------------------------------------------------------------
@@ -88,32 +88,31 @@ class MobileChecker {
 	}
 }
 
-
 /* -------------------------------------------------------------------------------------------------------------------------------------------------
 BaseHelpers
 -----------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 class BaseHelpers {
-    static html = document.documentElement;		
-    static addTouchClass() {
-        if (MobileChecker.isAny) {
-            BaseHelpers.html.classList.add('touch');
-        }
-    }
-    static addLoadedClass() {
-        window.addEventListener('load', () => {
-            setTimeout(() => {
-                BaseHelpers.html.classList.add('loaded');
-            }, 0);
-        });
-    }	
-    static get getHash() {
-        return location.hash?.replace('#', '');
-    }
-    static calcScrollbarWidth() {
-        const scrollbarWidth = (window.innerWidth - document.body.clientWidth) / 16 + 'rem';
-        BaseHelpers.html.style.setProperty('--bh-scrollbar-width', scrollbarWidth);
-    }
+	static html = document.documentElement;
+	static addTouchClass() {
+		if (MobileChecker.isAny) {
+			BaseHelpers.html.classList.add('touch');
+		}
+	}
+	static addLoadedClass() {
+		window.addEventListener('load', () => {
+			setTimeout(() => {
+				BaseHelpers.html.classList.add('loaded');
+			}, 0);
+		});
+	}
+	static get getHash() {
+		return location.hash?.replace('#', '');
+	}
+	static calcScrollbarWidth() {
+		const scrollbarWidth = (window.innerWidth - document.body.clientWidth) / 16 + 'rem';
+		BaseHelpers.html.style.setProperty('--bh-scrollbar-width', scrollbarWidth);
+	}
 }
 
 /* -------------------------------------------------------------------------------------------------------------------------------------------------
@@ -122,3 +121,69 @@ Init
 BaseHelpers.addLoadedClass();
 BaseHelpers.calcScrollbarWidth();
 BaseHelpers.addTouchClass();
+
+
+
+/* -------------------------------------------------------------------------------------------------------------------------------------------------
+ Back to top button
+-----------------------------------------------------------------------------------------------------------------------------------------------------*/
+class BackToTop {
+	constructor(el, opts = {}) {
+
+		// Проверка елемента на его тип, и коррекция... 
+		if (typeof el === 'string') {
+			this.button = document.querySelector(el);
+		} else if (el instanceof Element) {
+			this.button = el;
+		} else {
+			throw new Error('Неверный тип аргумента');
+		}
+		if (!this.button) {
+			throw new Error('Элемент не найден');
+		}
+
+		// Дефолтные опции
+		const defaultConfig = {
+			breakpoint: 500,
+			activeClass: "is-active",
+		};
+		this.options = Object.assign(defaultConfig, opts);
+		this.listener();
+	}
+
+	// Функция прокрутки
+	scrollToTop(e) {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
+
+	// Отслеживание кликов и скролинга
+	listener() {
+		this.button.addEventListener("click", (e) => this.scrollToTop(e));
+		window.addEventListener("scroll", (e) => this.checkPosition(e));
+		window.addEventListener('resize', this.updateSiteWidth);
+		this.updateSiteWidth();
+	}
+	updateSiteWidth() {
+		const container = document.querySelector('.container'); // твой главный блок
+		const width = container.offsetWidth;
+		document.documentElement.style.setProperty('--dynamic-site-width', width + 'px');
+	}
+
+	// Функция реагирования на положение скролинга
+	checkPosition(e) {
+		if (window.scrollY > this.options.breakpoint) {
+			this.button.classList.add(this.options.activeClass);
+		} else {
+			this.button.classList.remove(this.options.activeClass);
+		}
+	}
+}
+
+
+const BackToTopBtn = document.querySelector("#back-to-top");
+BackToTopBtn && new BackToTop(BackToTopBtn, {
+	// Рубеж в пикселях от верха экрана, при котором срабатывает появление и исчизновение кнопки
+	breakpoint: 700,
+	// Активный класс, который добавляется при прохождении рубежа (breakpoint)
+	activeClass: "is-active",
+});
